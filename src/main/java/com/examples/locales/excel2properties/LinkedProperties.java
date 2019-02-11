abff9af0847b17787470bb593607afb6f50dda8a
vinay shetty <vinayshetty20@gmail.com>
2019-02-11 12:47:20 +0530

    development completed
    excel sheet to locale properties file

---
+++ src/main/java/com/examples/locales/excel2properties/LinkedProperties.java
@@ -0,0 +1,45 @@
+package com.examples.locales.excel2properties;
+
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Properties;
+
+public class LinkedProperties extends Properties {
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+
+    private final HashSet<Object> keys = new LinkedHashSet<>();
+
+    public LinkedProperties() {
+        // Nothing is done here
+    }
+
+    public Iterable<Object> orderedKeys() {
+        return Collections.list(keys());
+    }
+
+    @Override
+    public Enumeration<Object> keys() {
+        return Collections.<Object>enumeration(keys);
+    }
+
+    @Override
+    public Object put(Object key, Object value) {
+        keys.add(key);
+        return super.put(key, value);
+    }
+
+    @Override
+    public synchronized boolean equals(Object o) {
+        return super.equals(o);
+    }
+
+    @Override
+    public synchronized int hashCode() {
+        return super.hashCode();
+    }
+}
